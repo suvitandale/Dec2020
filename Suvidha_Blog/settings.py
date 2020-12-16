49df61fd7825a24660d5b0582a3fbf829e5b46cc
Suvidha <suvi@gmail.com>
2020-12-16 16:00:53 +0530

    deployment changes2

--- Suvidha_Blog/settings.py
+++ Suvidha_Blog/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = '&t_e0w&@ev3vbl=_4xv7jq79#wi#*i06l$a+0yo5bns-3ch6c('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['suvitandale.herokuapp.com']
+ALLOWED_HOSTS = ['newblogapp12.herokuapp.com/']
 
 
 # Application definition
